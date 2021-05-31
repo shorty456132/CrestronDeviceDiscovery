ec6256cd0c27e536d9ca6bceed44c220e56ce25c
Andrew <a.laiacano@gmail.com>
2021-05-31 07:34:41 -0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace CrestronDevices
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnGetDevs = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.DimGray;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Ivory;
+            this.richTextBox1.Location = new System.Drawing.Point(249, 58);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(802, 604);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnGetDevs
+            // 
+            this.btnGetDevs.Location = new System.Drawing.Point(47, 58);
+            this.btnGetDevs.Name = "btnGetDevs";
+            this.btnGetDevs.Size = new System.Drawing.Size(143, 58);
+            this.btnGetDevs.TabIndex = 1;
+            this.btnGetDevs.Text = "Get Devices";
+            this.btnGetDevs.UseVisualStyleBackColor = true;
+            this.btnGetDevs.Click += new System.EventHandler(this.btnGetDevs_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1063, 687);
+            this.Controls.Add(this.btnGetDevs);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnGetDevs;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrestronDevices
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnGetDevs_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.Text += "getting devices\n";
+            UdpClient client = new UdpClient();
+            IPEndPoint IP = new IPEndPoint(IPAddress.Broadcast, 41794);
+            byte[] bytes = Encoding.ASCII.GetBytes("\x14\x00\x00\x00\x01\x04\x00\x03\x00\x00");
+            client.Send(bytes, bytes.Length, IP);
+            client.Close();
+            StartListening();
+        }
+
+        private readonly UdpClient udp = new UdpClient(41794);
+        private void StartListening()
+        {
+            try
+            {
+                this.udp.BeginReceive(new AsyncCallback(Receive), null);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void Receive(IAsyncResult ar)
+        {
+            IPEndPoint IP = new IPEndPoint(IPAddress.Any, 41796);
+            byte[] bytes = udp.EndReceive(ar, ref IP);
+            var data = Encoding.ASCII.GetString(bytes).Split(new[] { '\0' }, StringSplitOptions.RemoveEmptyEntries);
+
+            this.Invoke(new MethodInvoker(delegate
+            {
+                if (data.Length >= 5)
+                    richTextBox1.Text += $"[{IP.Address}] {data[3]} - {data[4]}\n";
+            }));
+
+            StartListening();
+        }
+    }
+}
